dbb571d72c2e06c55bdc2bac78bea51f7d074a50
IkasamaYeah <neconeco_festival@yahoo.co.jp>
2024-02-04 06:51:12 +0900

    no message

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 export function Home () {
   return(<>
-    <h1>Pecopecodex</h1>
+    <h1>Pecopecodexxxxxxxx</h1>
     <div>
       <Link to={`/dictionary`}><Button>図鑑</Button></Link>
       <Link to={`/combination`}><Button>配合</Button></Link>
