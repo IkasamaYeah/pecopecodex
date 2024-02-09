58a7e1fb38ede261526b26dcc4bf875a3a983ba3
IkasamaYeah <neconeco_festival@yahoo.co.jp>
2024-02-10 00:21:55 +0900

    no message

--- src/pages/combination.tsx
+++ src/pages/combination.tsx
@@ -18,28 +18,10 @@ const ResultCombinationWrapper = styled.div`
 const ReverseSelector = styled.select`
 `
 
-const InheritationSelector = styled.select`
-`
-
-const ResultInheritationWrapper = styled.div`
-`
-
 export function Combination() {
   const [pedigree, setPedigree] = useState("")
 
   const [reverse, setReverse] = useState("")
-  
-  const [inheritingPal, setInheritingPal] = useState("")
-  const inheritingPalObj = PalData.find(pal => pal.name === inheritingPal)
-  
-  const [inheritedPal,setInheritedPal] = useState("")
-  const inheritedPalObj = PalData.find(pal => pal.name === inheritedPal)
-  const inheritedPalParentObj = inheritedPalObj?.parent.map(([a,b]) => ([PalData.find(pal => pal.name === a),PalData.find(pal => pal.name === b)]))
-
-  const firstChildrens = PalData.filter(pal => pal.parent.flat().find(p => p === inheritingPal))                           //parentプロパティを参照し、継承元のパルの名前が含まれているものをフィルタリングしfirstChildrenに格納
-  const secondChildrens = PalData.filter(pal => pal.parent.flat().find(p => firstChildrens.map(f => f.name).includes(p)))  //firstChildrenの名前が含まれているものを格納
-  const thirdChildrens = PalData.filter(pal => pal.parent.flat().find(p => secondChildrens.map(f => f.name).includes(p)))  //secondChildrenの名前が含まれているものを格納
-  const fourthChildrens = PalData.filter(pal => pal.parent.flat().find(p => thirdChildrens.map(f => f.name).includes(p)))  //thirdChildrenの名前が含まれているものを格納
 
   const handlePedigreeSelect: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
     const value = (event.currentTarget.value)
@@ -49,16 +31,6 @@ export function Combination() {
   const handleReverseSelect: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
     const value = (event.currentTarget.value)
     setReverse(value)
-  }
-
-  const handleinheritingPalSelector: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
-    const value = (event.currentTarget.value)
-    setInheritingPal(value)
-  }
-
-  const handleinheritedPalSelector: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
-    const value = (event.currentTarget.value)
-    setInheritedPal(value)
   }
 
   return (<>
@@ -117,29 +89,5 @@ export function Combination() {
         </ResultCombinationWrapper>
       )
     })}
-
-    {/* 継承配合 */}
-      {/* 親決め */}
-      <InheritationSelector  name="" id="" onChange={handleinheritingPalSelector}>
-        <option value="">継承元のパルを選択</option>
-        {PalData.map((inheritationParent) => 
-          <option value={inheritationParent.name}>{inheritationParent.name}</option>
-        )}
-      </InheritationSelector>
-      
-      {/* 作りたいパル決め */}
-      <InheritationSelector name="" id="" onChange={handleinheritedPalSelector}>
-        <option value="">継承先のパルを選択</option>
-        {PalData.map((InheritedChild)=>
-          <option value={InheritedChild.name}>{InheritedChild.name}</option>
-        )}
-      </InheritationSelector>
-
-      {/* リストの結果表示 */}
-      <ResultInheritationWrapper>
-        {inheritedPalParentObj?.map(([a,b])=>
-          <p>{a?.name}+{b?.name}</p>
-        )}
-      </ResultInheritationWrapper>
   </>)
 }
