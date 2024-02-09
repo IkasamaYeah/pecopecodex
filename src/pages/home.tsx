f24d01511a0996394b32ea2e5dd56f1af7ccc77d
IkasamaYeah <neconeco_festival@yahoo.co.jp>
2024-02-10 00:18:34 +0900

    リンクをとりあえず用意

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,6 +7,7 @@ export function Home () {
     <div>
       <Link to={`/dictionary`}><Button>図鑑</Button></Link>
       <Link to={`/combination`}><Button>配合</Button></Link>
+      <Link to={`/inheritation`}><Button>継承</Button></Link>
     </div>
   </>)
 }
